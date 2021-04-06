7c334d7d38efe2894767a751e53a93e2965a0af9
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-06 15:55:51 -0700

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record){
+    //search through record for index of "w"
+    //return the year of that indexed value
 
+    //const year = record.indexOf("W");
+    //console.log(year);
+    //return record[year];
+    let win = record.find(recYear => recYear.result == "W")
+    return !!win ? win.year : undefined;
+}
+
+
+let win = record.find(record => record === "W")
